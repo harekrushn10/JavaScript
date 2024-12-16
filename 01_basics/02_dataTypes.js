e36245f1bd77befa491143c67d4a2cc5a9a5c216
HAREKRUSHN VAGHASIYA <harekrushnvaghasiya@gmail.com>
2024-12-16 17:52:59 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version 
+
+// alert(3 + 3) // we are using node.js not browser
+// Browser inspect as by default JavaScript engine in google 
+
+console.log(3 
+     + 
+        3) //code redability that should be high
+
+console.log("hari")
+
+let name = "hari"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint for more larger values
+// string => ""
+// boolean => true, false
+// null => stand-alone value
+// undefined => 
+// symbol => for uniquness
+
+// object 
+
+console.log(typeof "hari");//string
+console.log(typeof age);//number
+console.log(typeof undefined);
+console.log(typeof null);
+
+// typeof null => object
+// typeof undefined => undefined
